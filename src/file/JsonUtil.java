8fc7c2ae1374b109a430a0be4b9ad056bb1d0e83
tamata78 <deveenjhalf7@gmail.com>
2019-06-21 00:19:10 +0900

    init commit

---
+++ src/file/JsonUtil.java
@@ -0,0 +1,51 @@
+package file;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+
+	public static void main(String[] args) {
+		// JSONからJavaオブジェクトに変換
+		String testJson1 = "{\"id\":1, \"name\":\"taro\",\"sikaku\":[\"基本\",\"応用\"]}";
+		Syain syain1 = new Syain();
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			syain1 = mapper.readValue(testJson1, Syain.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// JavaオブジェクトからJSONに変換
+		List<String> sikaku = Arrays.asList("基本", "応用");
+		Syain syain2 = new Syain();
+		syain2.setId(1);
+		syain2.setN("taro");
+		syain2.setSikaku(sikaku);
+		ObjectMapper mapper2 = new ObjectMapper();
+
+		try {
+			String testJson2 = mapper2.writeValueAsString(syain2);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		// JSONのキー項目を指定して値を取得
+		try {
+			JsonNode node1 = mapper2.readTree(testJson1);
+			int id = node1.get("id").asInt();
+			String name = node1.get("name").asText();
+			String sikaku1 = node1.get("sikaku").get(0).asText();
+			String sikaku2 = node1.get("sikaku").get(1).asText();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/file/Syain.java
@@ -0,0 +1,31 @@
+package file;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Syain {
+	@JsonProperty("id")
+	private int id;
+
+	// @JsonPropertyの値がJSONの項目になる
+	// (nではなくnameになる)
+	@JsonProperty("name")
+	private String n;
+
+	@JsonProperty("sikaku")
+	private List<String> sikaku; // 資格
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setN(String n) {
+		this.n = n;
+	}
+
+	public void setSikaku(List<String> sikaku) {
+		this.sikaku = sikaku;
+	}
+
+}
