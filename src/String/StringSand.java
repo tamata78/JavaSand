692d92a3e5e33632d3ec5c3835b91a5f9c6979fe
tamata78 <deveenjhalf7@gmail.com>
2019-07-01 11:44:50 +0900

    exchange null  to emptyString

--- src/String/StringSand.java
+++ src/String/StringSand.java
@@ -1,10 +1,15 @@
 package String;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringSand {
 	public static void main(String[] args) {
-
+		// 前3文字切り取り
 		String res = "12345".substring(0, 3);
 		System.out.println(res);
 
+		// nullを空文字変換
+		String nullString =  StringUtils.defaultString(null);
+		System.out.println(":" + nullString + ":");
 	}
 }
