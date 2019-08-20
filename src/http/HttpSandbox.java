925844c56dfb9082d60409216ff1070e9c846a0f
tamata78 <5101956+tamata78@users.noreply.github.com>
2019-08-20 21:04:23 +0900

    fix build error

--- src/http/HttpSandbox.java
+++ src/http/HttpSandbox.java
@@ -9,57 +9,57 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
 public class HttpSandbox {
-	public static HttpServletResponse res;
+    public static HttpServletResponse res;
 
     public static void main(String[] args) {
-		// 外部API Get
-		try {
-			HttpURLConnection conn = HttpUtils.connectGet("http://www.XXXXXXXXX.xxx/servPersonalInfo", "text/plain");
-			HttpUtils.execApiGet(conn);
+        // 外部API Get
+        try {
+            HttpURLConnection conn = HttpUtils.connectGet("http://www.XXXXXXXXX.xxx/servPersonalInfo", "text/plain");
+            HttpUtils.execApiGet(conn);
 
-			System.out.println("ヘッダー要素[" + conn.getHeaderFields() + "]");
-			System.out.println("レスポンスメッセージ[" + conn.getResponseMessage() + "]");
-			if (HttpURLConnection.HTTP_OK != conn.getResponseCode()) {
-				// error log
-			}
+            System.out.println("ヘッダー要素[" + conn.getHeaderFields() + "]");
+            System.out.println("レスポンスメッセージ[" + conn.getResponseMessage() + "]");
+            if (HttpURLConnection.HTTP_OK != conn.getResponseCode()) {
+                // error log
+            }
 
-			// レスポンスがXMLの場合
-			Document doc = HttpUtils.getApiResXml(conn);
-			NodeList nodes = doc.getElementsByTagName("response1");
-			for (int i = 0; i < nodes.getLength(); i++) {
-				System.out.println(nodes.item(i).getTextContent());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            // レスポンスがXMLの場合
+            Document doc = HttpUtils.getApiResXml(conn);
+            NodeList nodes = doc.getElementsByTagName("response1");
+            for (int i = 0; i < nodes.getLength(); i++) {
+                System.out.println(nodes.item(i).getFirstChild().getNodeValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// 外部API Post
-		try {
-			HttpURLConnection conn = HttpUtils.connectPost("http://www.XXXXXXXXX.xxx/servPersonalInfo",
-					"application/json; charset=UTF-8");
-			String json = "";
-			HttpUtils.execApiPost(conn, json);
+        // 外部API Post
+        try {
+            HttpURLConnection conn = HttpUtils.connectPost("http://www.XXXXXXXXX.xxx/servPersonalInfo",
+                    "application/json; charset=UTF-8");
+            String json = "";
+            HttpUtils.execApiPost(conn, json);
 
-			System.out.println("ヘッダー要素[" + conn.getHeaderFields() + "]");
-			System.out.println("レスポンスメッセージ[" + conn.getResponseMessage() + "]");
-			if (HttpURLConnection.HTTP_OK != conn.getResponseCode()) {
-				// error log
-			}
+            System.out.println("ヘッダー要素[" + conn.getHeaderFields() + "]");
+            System.out.println("レスポンスメッセージ[" + conn.getResponseMessage() + "]");
+            if (HttpURLConnection.HTTP_OK != conn.getResponseCode()) {
+                // error log
+            }
 
-			// レスポンスがJsonの場合
-			String resJson = HttpUtils.getApiResJson(conn);
-			System.out.println("レスポンスJson[" + resJson + "]");
+            // レスポンスがJsonの場合
+            String resJson = HttpUtils.getApiResJson(conn);
+            System.out.println("レスポンスJson[" + resJson + "]");
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// Cookie
-		Cookie cookie = new Cookie("userId", "111");
-		cookie.setPath("http://example.com");
+        // Cookie
+        Cookie cookie = new Cookie("userId", "111");
+        cookie.setPath("http://example.com");
 
-		// レスポンスにcookieを追加
-		res.addCookie(cookie);
+        // レスポンスにcookieを追加
+        res.addCookie(cookie);
 
-	}
+    }
 }
