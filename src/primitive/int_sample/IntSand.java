7a96de28ccc71038d0a1ae8a5cc667a8a5660293
tamata78 <5101956+tamata78@users.noreply.github.com>
2019-08-22 12:01:19 +0900

    add int_sample

---
+++ src/primitive/int_sample/IntSand.java
@@ -0,0 +1,10 @@
+package primitive.int_sample;
+
+public class IntSand {
+    public static void main(String[] args) {
+        // 10進以外の整数表現
+        int octalNum = 010; // 8進数：10進の8
+        int hexNum = 0xA; // 16進数：10進の10
+    }
+
+}
