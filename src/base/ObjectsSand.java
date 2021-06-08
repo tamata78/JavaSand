b585b53545fe8cada7b39fe043dacddd54a8dd34
tamata78 <5101956+tamata78@users.noreply.github.com>
2021-06-08 13:03:33 +0900

    add ObjectsSand

---
+++ src/base/ObjectsSand.java
@@ -0,0 +1,23 @@
+package base;
+
+import base.bean.NumArrayList;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ObjectsSand {
+    public static void main(String[] args) {
+        // Objects.requireNonNull
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now2 = Objects.requireNonNull(now);
+
+        Boolean boolTrue1 = Objects.isNull(null); // nullの場合
+        Boolean boolTrue2 = Objects.nonNull(new ArrayList<String>); // null以外の場合
+
+        Object obj = null;
+        System.out.println(Objects.toString(obj, "nullです。")); // 第二引数はなしでもOK
+    }
+}
+
